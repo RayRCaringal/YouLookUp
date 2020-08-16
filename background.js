36208078a2a3a06c44f9ca290bad342bc44fce64
RayRCaringal <rayrcaringal@gmail.com>
2020-08-16 17:08:10 -0400

    Background Script Depreciated

--- background.js
+++
@@ -1,7 +0,0 @@
-window.playlists = {}
-chrome.runtime.onMessage.addListener(function ( request, sender) {
-    if(request.from == 'content' && request.target == 'background'){
-        window.playlists[request.url] = request.myStorage;
-    }
-   
-});
